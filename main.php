b633cd9943bcf29653b450c144b86919820a50ec
JPJAP <porajap@users.noreply.github.com>
2019-07-27 16:12:20 +0700

    ss

--- main.php
+++ main.php
@@ -426,12 +426,14 @@ switch ($PmID) {
 
     }
 
+  //==========================================================
     window.addEventListener("beforeunload", function (e) {
         var data = {
           'STATUS': 'UpdateActive'
         };
         senddata(JSON.stringify(data));
     });
+  //==========================================================
 
     function logoff(chk) {
       if(chk == 1){
