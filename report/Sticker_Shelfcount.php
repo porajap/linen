6107868ebeb96e93ce9177428abdbc29df5e6ce1
mhee2555 <sarayuth.mhee@gmail.com>
2019-09-11 15:22:47 +0700

    f

--- report/Sticker_Shelfcount.php
+++ report/Sticker_Shelfcount.php
@@ -42,21 +42,104 @@ $count = 0;
 // echo '<br>TotalQty : '.$TotalQty ;
 // echo '<br>sendQty : '.$sendQty ;
 
+// $loop1 = floor($TotalQty/$sendQty);
+// for($i=1;$i<=$loop1;$i++){
+//   echo '<br>'.$i.'. ItemCode : '. $ItemCode;
+//   echo '<br>TotalQty : '. $sendQty;
+//   echo '<br><hr>';
+// }
 // echo "<br>";
+// $loop2 = $loop1*$sendQty;
+// if($loop2<$TotalQty){
+//   $lass = $TotalQty - $loop2;
+//   echo '<br>'.$i.'. ItemCode : '. $ItemCode;
+//   echo '<br>TotalQty : '. $lass;
+//   echo '<br><hr>';
+// }
+
+
+
+/**
+ * @package com.tecnick.tcpdf
+ * @abstract TCPDF - Example: 2D barcodes.
+ * @author Nicola Asuni
+ * @since 2008-03-04
+ */
+
+// Include the main TCPDF library (search for installation path).
+require_once('../tcpdf/tcpdf.php');
+
+// create new PDF document
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, array(50,45), true, 'UTF-8', false);
+
+
+$pdf->SetTitle('TCPDF Example 050');
+
+// set default monospaced font
+$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+// set margins
+$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+// set auto page breaks
+$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+$pdf->setPrintHeader(false);
+$pdf->setPrintFooter(false);
+// set image scale factor
+$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+// set some language-dependent strings (optional)
+if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
+    require_once(dirname(__FILE__).'/lang/eng.php');
+    $pdf->setLanguageArray($l);
+}
+
+// ---------------------------------------------------------
+
+// NOTE: 2D barcode algorithms must be implemented on 2dbarcode.php class file.
+
+// set font
+$pdf->SetFont('helvetica', '', 3);
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// set style for barcode
+$style = array(
+    'border' => false,
+    'vpadding' => 'auto',
+    'hpadding' => 'auto',
+    'fgcolor' => array(0,0,0),
+    'bgcolor' => false, //array(255,255,255)
+    'module_width' => 1, // width of a single module in points
+    'module_height' => 1 // height of a single module in points
+);
+
 $loop1 = floor($TotalQty/$sendQty);
 for($i=1;$i<=$loop1;$i++){
-  echo '<br>'.$i.'. ItemCode : '. $ItemCode;
-  echo '<br>TotalQty : '. $sendQty;
-  echo '<br><hr>';
+  $pdf->AddPage();
+  $html = 'itemcode :'.$ItemCode.'';
+  $pdf->writeHTML($html, true, false, true, false, '');
+  $pdf->write2DBarcode('www.tcpdf.org', 'QRCODE,L', 17,10, 5, 5, $style, 'N');
+  // $pdf->Text(20, 25, 'QRCODE L');
+  // echo '<br>'.$i.'. ItemCode : '. $ItemCode;
+  // echo '<br>TotalQty : '. $sendQty;
+  // echo '<br><hr>';
 }
-echo "<br>";
 $loop2 = $loop1*$sendQty;
 if($loop2<$TotalQty){
-  $lass = $TotalQty - $loop2;
-  echo '<br>'.$i.'. ItemCode : '. $ItemCode;
-  echo '<br>TotalQty : '. $lass;
-  echo '<br><hr>';
+  // $pdf->AddPage();
+  $html = 'itemcode :'.$ItemCode.'';
+  $pdf->writeHTML($html, true, false, true, false, '');
+  $pdf->write2DBarcode('www.tcpdf.org', 'QRCODE,L', 20, 5, 15, 20, $style, 'N');
+  // $pdf->Text(20, 25, 'QRCODE L');
 }
 
 
+//Close and output PDF document
+$pdf->Output('example_050.pdf', 'I');
 
+//============================================================+
+// END OF FILE
+//============================================================+
