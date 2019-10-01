56a3cf83297348eb53fcc6c850db53bae513d48f
mhee2555 <sarayuth.mhee@gmail.com>
2019-10-01 15:58:48 +0700

    F

--- process/sendMail_percent.php
+++ process/sendMail_percent.php
@@ -13,6 +13,9 @@ $Total1 = $_POST['Total1'];
 $Total2 = $_POST['Total2'];
 $Precent = $_POST['Precent'];
 $email = $_POST['email'];
+$DocNoC = $_POST['DocNoC'];
+$DocNoD = $_POST['DocNoD'];
+$HptNameTH = $_POST['HptNameTH'];
 
 // if($dateDiff == 30){
 //     $update_alert = "UPDATE contract_parties_hospital SET day_30 = 1 WHERE RowID = '$RowID'";
@@ -26,15 +29,19 @@ $body = '
 <body>
 <br>
 ___________________________________________________________________<br>
-<h3>'.$HptName.'</h3>
+<h3>'.$HptNameTH.'</h3>
+<b>เลขที่เอกสารผ้าสกปรก:</b> '.$DocNoD.'<br> 
 <b>นํ้าหนักผ้าสกปรก:</b> '.$Total1.'<br>
+<b>เลขที่เอกสารผ้าสะอาด:</b> '.$DocNoC.'<br>
 <b>นํ้าหนักผ้าสะอาด:</b> '.$Total2.'<br>
 <b>เปอร์เซ็นที่ได้:</b> '.$Precent.' <br>
 ___________________________________________________________________<br>
 <h3>'.$HptName.'</h3>
+<b>DOCNO DIRTY:</b> '.$DocNoD.'<br> 
 <b>WEIGHT DIRTY:</b> '.$Total1.'<br>
-<b>WEIGHT CLEAN:</b> '.$Total2.'<br>
-<b>PER CENT:</b> '.$Precent.' <br>
+<b>DOCNO CLEAN:</b> '.$DocNoC.'<br>
+<b>WEIGHT CIEAN:</b> '.$Total2.'<br>
+<b>PERCENT:</b> '.$Precent.' <br>
 ___________________________________________________________________<br>
 </body>
 </html>
